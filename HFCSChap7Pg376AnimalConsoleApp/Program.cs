7433768d7276d9daf690a8486411c3422f6cf8fc
Sean McKinney <seanamckinney8@gmail.com>
2022-05-16 09:14:02 -0500

    Add project files.

---
+++ HFCSChap7Pg376AnimalConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFCSChap7Pg376AnimalConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
