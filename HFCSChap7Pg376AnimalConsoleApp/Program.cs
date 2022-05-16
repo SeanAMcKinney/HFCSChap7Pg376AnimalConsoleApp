ebd805b20450b0a1066fd837260315d78ceace8f
Sean McKinney <seanamckinney8@gmail.com>
2022-05-16 09:26:55 -0500

    init commit:  Created a class structure starting with animal.  Hippo and Canine inherited from Animal.  Wolf inherited from Canine.

--- HFCSChap7Pg376AnimalConsoleApp/Program.cs
+++ HFCSChap7Pg376AnimalConsoleApp/Program.cs
@@ -6,10 +6,58 @@ using System.Threading.Tasks;
 
 namespace HFCSChap7Pg376AnimalConsoleApp
 {
+    abstract class Animal
+    {
+        public abstract void MakeNoise();
+    }//end class
+
+    class Hippo : Animal
+    {
+        public override void MakeNoise()
+        {
+            Console.WriteLine("Grunt");
+        }
+    }//end class
+
+    abstract class Canine : Animal
+    {
+        public bool BelongsToPack { get; protected set; } = false;
+    }//end class
+
+    class Wolf : Canine
+    {
+        public Wolf(bool belongsToPack)
+        {
+            BelongsToPack = belongsToPack;
+        }
+
+        public override void MakeNoise()
+        {
+            if (BelongsToPack)
+            {
+                Console.WriteLine("I'm in a pack.");
+                Console.WriteLine("Aroooooo!");
+            }         
+        }
+
+        public void HuntInPack()
+        {
+            if (BelongsToPack)
+            {
+                Console.WriteLine("I'm going hunting with my pack!");
+            }
+            else
+            {
+                Console.WriteLine("I'm not in a pack.");
+            }
+        }
+    }//end class
+
+
     class Program
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+        }//end main
+    }//end class
+}//end namespace
